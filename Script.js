3cc131b1bbc9282498ba3a923578c2fa735b1fbc
Brandon Rozek <maxo2708@users.noreply.github.com>
2013-08-24 00:52:18 -0400

    Initial Commit

---
+++ Script.js
@@ -0,0 +1,52 @@
+var Total = 0
+
+$(document).ready(function () {
+    //Hides main application
+    $('#Overlay2').hide();
+
+    //Hides Start Screen and Shows Application
+    $('#OverlayButton').click(function () {
+        $('#Overlay1').fadeOut('slow');
+        $('#Overlay2').fadeIn('slow');
+    })
+
+    //Prevents the use of the 'Enter' key
+    $('input').keypress(function (event) {
+        if (event.keyCode == 13) {
+            event.preventDefault();
+        }
+    })
+
+    $('#Submit').click(function () {
+
+        //Declare Variables
+        var ItemName = $('input[name=ItemName]').val();
+        var PriceVal = $('input[name=PriceVal]').val();
+
+        //Verifies that the value in the 'Price Box' is valid
+        if (PriceVal == "" || PriceVal == null) {
+            alert("Please Enter in a Price");
+        }
+        else if (isNaN(PriceVal)) {
+            alert("The value in the Price Box is Not a Number");
+        }
+        else {
+            $('#UsrPrice').append('<p class="item">' + PriceVal + '</p>');
+            
+            //Calculates current total
+            Total += Number(PriceVal);
+            $('#LogTotal').html('Total: ' + Total);
+            
+            //Checks items name then appends it
+            if (ItemName == "" || ItemName == null) {
+                $('#UsrItem').append('<p class="item"> Unidentified Item </p>');
+            }
+            else {
+                $('#UsrItem').append('<p class="item">' + ItemName + '</p>');
+            }
+        }
+    }
+    )
+
+
+})
