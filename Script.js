4a80f83a9259216d0399f3ce8b64e16a82e5749f
maxo2708 <maxo2708@users.noreply.github.com>
2013-09-05 20:16:41 -0400

    v0.6

--- Script.js
+++ Script.js
@@ -1,111 +1,152 @@
-//Declaring functions and variables until $(document).ready();
-var Total = 0
+//Setup Cash Register Object
+var CashRegister = {
 
-//Code for item submission
-var Submit = function () {
     //Declare Variables
-    var ItemName = $('input[name=ItemName]').val();
-    var PriceVal = $('input[name=PriceVal]').val();
-    //Verifies that the value in the 'Price Box' is valid
-    if (PriceVal == "" || PriceVal == null) {
-        alert("Please Enter in a Price");
-    }
-    else if (isNaN(PriceVal)) {
-        alert("The value in the Price Box is Not a Number");
-    }
-    else {
-        $('#Price').append('<p class="item">' + PriceVal + '</p>');
-        //Calculates current total
-        Total += Number(PriceVal);
-        $('#LogTotal').html('Total: ' + Total);
-        //Checks items name then appends it
-        if (ItemName == "" || ItemName == null) {
-            $('#Item').append('<p class="item"> Unidentified Item </p>');
-        }
-        else {
-            $('#Item').append('<p class="item">' + ItemName + '</p>');
-        }
-    }
-}
+    Subtotal: 0,
+    Total: 0,
+    Currency: undefined,
+    Tax: undefined,
 
-//Code for Checkout
-var Checkout = function () {
-    //Hide and Show some GUI for Checkout
-    $('#Submit').off();
-    $('#UsrItem').hide();
-    $('#UsrPrice').hide();
-    $('#Checkout').hide();
-    $('#UsrOwe').show();
-    $('#UsrPaid').show();
-    $('#Reset').show();
-    //Checks the Amount paid and shows AmtLeft or Change
-    $('#Submit').on("click", function () {
-        var AmtPaid = $('input[name=UsrPaid]').val();
-        var Change = AmtPaid - Total;
-        if (AmtPaid == null || AmtPaid == "") {
-            alert("Please insert the amount paid.");
-        }
-        else if (isNaN(AmtPaid)) {
-            alert("Please insert the amount paid in a numerical form.");
-        }
-        else if (Change < 0) {
-            $('#Change').html('Amount Left: ' + Math.abs(Change));
-        }
-        else {
-            $('#Change').html('Change: ' + Change);
-        }
+    //Declare Functions
+    Start: function () {
+        //Hides main application and other overlays
+        $('#Overlay3').hide();
+        $('#Overlay2').hide();
+        $('#UsrOwe').hide();
+        $('#UsrPaid').hide();
+        $('#Reset').hide();
+        //Setup Event Handlers
+        $('#OverlayButton').click(CashRegister.Overlayer);
+        $('#SetupSubmit').on('click', CashRegister.Setup);
+        $('#Submit').on("click", CashRegister.Submit);
+        $('#Checkout').on("click", CashRegister.Checkout);
+        $('#Reset').on("click", CashRegister.Reset);
+    },
 
-    })
-}
-
-//Code for reset
-var Reset = function () {
-    //Configure Event Handlers
-    $('#Submit').off();
-    $('#Submit').on("click", Submit);
-    //Configure GUI for application reset
-    $('#UsrOwe').hide();
-    $('#UsrPaid').hide();
-    $('#Reset').hide();
-    $('#UsrItem').show();
-    $('#UsrPrice').show();
-    $('#Checkout').show();
-    //Reset previous entries and changes
-    $('.item').remove();
-    Total = 0;
-    $('#LogTotal').html("Total: ");
-}
-
-//Starting Code
-$(document).ready(function () {
-    //Hides main application and other overlays
-    $('#Overlay2').hide();
-    $('#UsrOwe').hide();
-    $('#UsrPaid').hide();
-    $('#Reset').hide();
-
-    //Hides Start Screen and Shows Application
-    $('#OverlayButton').click(function () {
+    //The 'start' button
+    Overlayer: function () {
         $('#Overlay1').fadeOut('slow');
-        $('#Overlay2').fadeIn('slow');
-    })
+        $('#Overlay3').fadeIn('slow');
+    },
 
-    //Prevents the use of the 'Enter' key
-    $('input').keypress(function (event) {
-        if (event.keyCode == 13) {
-            event.preventDefault();
+    //Setup Page 
+    Setup: function () {
+        //Update Variables
+        CashRegister.Currency = $('select[name=currency]').val();
+        CashRegister.Tax = $('input[name=Tax]').val();
+        //Make sure input is valid
+        if (CashRegister.Currency == null || CashRegister.Currency == "") {
+            alert("Please select a Currency");
         }
-    })
+        else {
+            console.log(CashRegister.Currency);
+        }
+        if (CashRegister.Tax == null || CashRegister.Tax == "") {
+            alert("Please fill in the tax");
+            return false;
+        }
+        else if (isNaN(CashRegister.Tax)) {
+            alert("Please insert the tax in a numerical form.");
+        }
+        else {
+            console.log(CashRegister.Tax); //For debug purposes
+        }
+        //Update GUI
+        $('#Overlay3').hide();
+        $('#Overlay2').show();
+        $('input').keyup(CashRegister.SubmitEnter);
+    },
 
-    //Call Submit and Checkout function when event occurs
-    $('#Submit').on("click", Submit);
-    $('#Checkout').on("click", Checkout);
-    $('#Reset').on("click", Reset)
+    //Repurpose use of 'enter' key
+    SubmitEnter: function (event) {
+        if (event.keyCode == 13) {
+            $("#Submit").click();
+        }
+    },
 
-}) 
+    //Code for item submission
+    Submit: function () {
+        //Declare Variables
+        CashRegister.ItemName = $('input[name=ItemName]').val();
+        CashRegister.PriceVal = $('input[name=PriceVal]').val();
+        //Verifies that the value in the 'Price Box' is valid
+        if (CashRegister.PriceVal == "" || CashRegister.PriceVal == null) {
+            alert("Please Enter in a Price");
+        }
+        else if (isNaN(CashRegister.PriceVal)) {
+            alert("The value in the Price Box is Not a Number");
+        }
+        else {
+            $('#Price').append('<p class="item">' + CashRegister.PriceVal + '</p>');
+            //Calculates and shows current subtotal
+            CashRegister.Subtotal += Number(CashRegister.PriceVal);
+            $('#LogSubtotal').html('Subtotal: ' + CashRegister.Currency + CashRegister.Subtotal);
+            //Calculates and shows current tax
+            Tax = Number(CashRegister.Tax) / 100
+            $('#LogTax').html('Tax: ' + CashRegister.Currency + (Tax * CashRegister.Subtotal))
+            //Calculates and shows current total
+            CashRegister.Total = Tax * CashRegister.Subtotal + CashRegister.Subtotal;
+            $('#LogTotal').html('Total: ' + CashRegister.Currency + CashRegister.Total)
+            //Checks items name then appends it
+            if (CashRegister.ItemName == "" || CashRegister.ItemName == null) {
+                $('#Item').append('<p class="item"> Unidentified Item </p>');
+            }
+            else {
+                $('#Item').append('<p class="item">' + CashRegister.ItemName + '</p>');
+            }
+        }
+    },
 
-//For debug purposes
-/*$(document).ready(function () {
-    $('#Overlay1').hide();
-    $('#Overlay2').hide();
-})*/
+    //Code for checkout
+    Checkout: function () {
+        //Hide and Show some GUI for Checkout
+        $('#Submit').off();
+        $('#UsrItem').hide();
+        $('#UsrPrice').hide();
+        $('#Checkout').hide();
+        $('#UsrOwe').show();
+        $('#UsrPaid').show();
+        $('#Reset').show();
+        //Checks the Amount paid and shows AmtLeft or Change
+        $('#Submit').on("click", function () {
+            var AmtPaid = $('input[name=UsrPaid]').val();
+            var Change = AmtPaid - CashRegister.Total;
+            if (AmtPaid == null || AmtPaid == "") {
+                alert("Please insert the amount paid.");
+            }
+            else if (isNaN(AmtPaid)) {
+                alert("Please insert the amount paid in a numerical form.");
+            }
+            else if (Change < 0) {
+                $('#Change').html('Amount Left: ' + Math.abs(Change));
+            }
+            else {
+                $('#Change').html('Change: ' + Change);
+            }
+        })
+    },
+
+    //Code for reset
+    Reset: function () {
+        //Configure Event Handlers
+        $('#Submit').off();
+        $('#Submit').on("click", CashRegister.Submit);
+        //Configure GUI for application reset
+        $('#UsrOwe').hide();
+        $('#UsrPaid').hide();
+        $('#Reset').hide();
+        $('#UsrItem').show();
+        $('#UsrPrice').show();
+        $('#Checkout').show();
+        //Reset previous entries and changes
+        $('.item').remove();
+        CashRegister.Total = 0;
+        CashRegister.Subtotal = 0;
+        $('#LogSubtotal').html("Subtotal: ");
+        $('#LogTax').html("Tax: ");
+        $('#LogTotal').html("Total: ")
+    }
+}
+
+//Start code
+$(document).ready(CashRegister.Start);
